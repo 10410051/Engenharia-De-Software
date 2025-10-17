178b31d75694785aa5b75e8602e2859162bd04bb
10410051 <226614468+10410051@users.noreply.github.com>
2025-10-17 20:20:03 -0300

    Init

--- Aula10/Login.java
+++ Aula10/Login.java
@@ -1,19 +1,25 @@
-public class Login {  
-  public static void main(String[] args) {
+package Aula10;
 
+import java.util.Scanner;
+
+
+public class Login {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
     String user = args[0];
     String pass = args[1];
-
+  
     if (user.equals("admin") && pass.equals("123")) {
-
+  
       System.out.println("Bem-vindo!");
-
+  
     } else {
-
+  
       System.out.println("Erro");
-
+  
     }
-
-  }
+  
+  } 
 
 }
